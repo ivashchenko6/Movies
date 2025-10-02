48d14d1b764cdca41753133b38ee5797aa52e018
Kyryl Ivashchenko <love2dance@Kyryls-Laptop.local>
2025-10-02 13:39:24 -0700

    http hook for the requests

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = async(url) => {
+        setLoading(true);
+        const API_KEY = process.env.API_KEY;
+
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${API_KEY}`,
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(
+                    `Something went wrong! Could not fetch ${url}. Status: ${response.status}`,
+                );
+            }
+
+            const data = await response.json();
+            setLoading(false);
+            return data;
+        } catch (e) {
+            setLoading(false);
+            setError(e.message);
+            throw e;
+        }
+    };
+
+    const cleanError = () => setError(null);
+
+    return { loading, error, cleanError, request };
+};
