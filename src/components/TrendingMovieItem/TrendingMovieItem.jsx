34fd398257c066b6a23740cd118b42f9ae5979fa
Kyryl  Ivashchenko <kirill040605@gmail.com>
2025-10-14 14:59:57 -0700

    trending list is ready

--- src/components/TrendingMovieItem/TrendingMovieItem.jsx
+++ src/components/TrendingMovieItem/TrendingMovieItem.jsx
@@ -1,17 +1,19 @@
 import './trendingMovieItem.scss';
 
 import starIcon from '../../assets/icons/star.png';
+import { Link } from 'react-router-dom';
 
 const TrendingMovieItem = ({ movieItem }) => {
     const {
         poster_path: movieImg,
         genre_ids,
         original_title: title,
+        id: movieId,
         overview,
-        popularity,
         release_date,
         vote_average,
         vote_count,
+        original_language
     } = movieItem;
     return (
         <li className='trending-movies__list-item'>
@@ -23,7 +25,7 @@ const TrendingMovieItem = ({ movieItem }) => {
             <div className='trending-movies__list-item__about'>
                 <div className='trending-movies__list-item__header'>
                     <p className='trending-movies__list-item__title'>
-                        {title.length < 22 ? title : title.slice(0, 22) + '...'}
+                        {title.length < 20 ? title : title.slice(0, 20) + '...'}
                     </p>
                     <div className='trending-movies__list-item__rating-wrapper'>
                         <p className='trending-movies__list-item__average'>
@@ -38,9 +40,48 @@ const TrendingMovieItem = ({ movieItem }) => {
                 </div>
 
                 <div className='trending-movies__list-item__body'>
-                    {overview}
+                    <div>
+                        <span className="movie-data__span">Description: </span>
+                        <p className="trending-movies__list-item__body-description">
+                            {overview}
+                        </p>
+                    </div>
+                    <div>
+                        <span className="movie-data__span">Original title: </span>
+                        <p className="trending-movies__list-item__body-original-title">
+                            {title}
+                        </p>
+                    </div>
+                    <div>
+                        <span className="movie-data__span">Original language: </span>
+                        <p className="trending-movies__list-item__body-original-language">
+                            {original_language}
+                        </p>
+                    </div>
+
+                    <div>
+                        <span className="movie-data__span">Genres: </span>
+                        <p className="trending-movies__list-item__body-genres">
+                            ...
+                        </p>
+                    </div>
+                    <div>
+                        <span className="movie-data__span">Vote Count: </span>
+                        <p className="trending-movies__list-item__body-vote-count">
+                            {vote_count}
+                        </p>
+                    </div>
+                    <div>
+                        <span className="movie-data__span">Realease Date: </span>
+                        <p className="trending-movies__list-item__body-realease-date">
+                            {release_date}
+                        </p>
+                    </div>
+                    <Link to={`/movies/${movieId}`} className="about-movie__button glow-on-hover">About</Link>
                 </div>
+
             </div>
+
         </li>
     );
 };
